7b2ebf21641735d527f33507f9b4ca63b445ffa4
Ben Arnold <ben@arnoldfamily.me.uk>
2024-04-03 07:41:53 +0100

    First document

---
+++ Context.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SqlJsonMigration;
+
+public class Context : DbContext
+{
+    public DbSet<MyDocument> Documents { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        optionsBuilder.UseSqlServer("Server=(localdb)\\MSSQLLocalDb;Database=SqlJsonMigration;");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<MyDocument>()
+            .OwnsOne(e => e.Json, builder => builder.ToJson());
+    }
+}
+
+public class MyDocument
+{
+    public int Id { get; set; }
+    public required TheJsonDocument Json { get; set; }
+}
+
+public class TheJsonDocument
+{
+    public required string Property1 { get; init; }
+    public required int Property2 { get; set; }
+}
---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SqlJsonMigration;
+
+var context = new Context();
+
+Console.Write("Load existing documents... ");
+var existing = await context.Documents.ToListAsync();
+Console.WriteLine($"found {existing.Count}");
+
+if (existing.Count == 0)
+{ 
+    Console.WriteLine($"Add a new document");
+    context.Documents.Add(new MyDocument { Json = new() { Property1 = "hello", Property2 = 43 } });
+    await context.SaveChangesAsync();
+}
+else
+{
+}
